123336a314293dacf6319f088df5b2d53a72ad8e
carlos-dagua27 <carlosdagua87@gmail.com>
2026-01-07 18:20:11 -0500

    Inicial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void suma () {
+        System.out.println("Prueba de git");
+        int val1 = 6;
+        int val2 = 10;
+        int resultado = val1 + val2;
+        System.out.println(resultado);
+    }
+    public static void main(String[] args) {
+        suma();
+    }
+}
