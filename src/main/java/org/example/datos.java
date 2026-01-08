a9f70a735236ecd4712ae58523a4ca3cff701742
carlos-dagua27 <carlosdagua87@gmail.com>
2026-01-07 19:56:15 -0500

    datos

--- src/main/java/org/example/datos.java
+++ src/main/java/org/example/datos.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class datos {
+    String Nombre = "Carlos";
+    int Edad = 24;
+    String Documento = "32093424";
+
+    public void salida(){
+        System.out.println(Nombre + Edad + Documento);
+    }
 }
